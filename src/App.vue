6f3640ccaa32ee3e8d5e4c3545a89d77a81c007b
Emilia Alves <enbrakaffe@gmail.com>
2024-03-31 12:16:01 -0300

    ready. set. go..

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <v-container>
+		<div class="v-row">
+			<div class="v-col-6 v-align-self-start">
+				<img class="tarffieImg" src="./assets/eri.jpg" alt="picture of Eri from Tatsuki Fujimoto Manga stylized with dog ears and Emacs & transgender flag bottom pins"/>
+			</div>
+			<div class="v-col-6">
+				test
+			</div>
+		</div>
+	</v-container>
+
+</template>
+
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+<template>
+  <v-icon icon="mdi-home" />
+</template>
+
