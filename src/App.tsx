9759eb6655e7ae257fb1aa3379321c0553fbc7fc
yoru <yoru@mothership.mothership.one>
2024-05-23 14:59:00 -0300

    back to work

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,93 @@ import Paper from '@mui/material/Paper';
 import './App.css';
 
 // * icon imports
-import CodeIcon from '@mui/icons-material/Code';
-
-
-// * importing pictures 
-import Denji from './assets/images/denji.jpg';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// * importing pictLeft ures 
+import Yoru from './assets/images/yoru.jpg';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: 'transparent',
   ...theme.typography.body2,
   padding: theme.spacing(0),
-  textAlign: 'center',
+  textAlign: 'justify',
   color: theme.palette.text.secondary,
 }));
 
 
-
 function App() {
   return (
-	<Grid container spacing={2}>
-		<Item xs={12} md={6}>
-			<img src={Denji} alt='Picture of manga "Chainsaw Man" by Tatsuki Fujimoto, showing Denji drinking from a mug'
-			style={{
-				width: '250px',
-				height: 'auto',
-				borderRadius: '20',
-				paddingLeft: '13px',
-			}}/>
-			</Item>
-			<Item>
-				<ul>
-					<li><CodeIcon/><a href="https://github.com/tarffie"> Github Profile</a></li>
-					<li></li>
-					<li></li>
-				</ul>
-			</Item>
-		</Grid>
+    <Grid container spacing={2}>
+    <Item xs={12} md={6}>
+    <div style={{
+      width: '50%',
+      display: "block",
+      marginLeft : 'auto',
+      marginRight : 'auto',
+    }}>
+    <img src={Yoru} alt='Picture of manga "Chainsaw Man" by Tatsuki Fujimoto, showing Yoru'
+    style={{
+      borderRadius: '25%',
+      width: '250px',
+          }}/>
+    </div>
+    </Item>
+    <Box component="section" sx={{ p: 2, borderBottom: '1px solid dark-grey', width : "100%"}}>
+    <Item>
+    <ul className="mainLinks" style={{
+      listStyle: "none",
+      paddingLeft: "0px",
+      textAlign: 'center',
+    }}>
+      <li><a href="https://github.com/tarffie" target="_blank"><GitHubIcon /></a></li>
+      <li><a href="https://x.com/tarffie" target="_blank"><XIcon /></a></li>
+      <li><a href="https://www.linkedin.com/in/tarffie/" target="_blank"><LinkedInIcon /></a></li>
+    </ul>
+    </Item>
+    </Box>
+    <Item className="introPar" style={{
+      color: "#fff",
+    }}>
+    <Box component="section" sx={{ p: 2}}>
+    <h2 className="fira-mono-medium" style={{ textAlign: "left", }}>Hi, I'm Yoru 👋</h2> 
+    <p className="fira-mono-regular" style={{
+      fontSize: '18px',
+    }}> 
+      My name is Yoru and I am a software programmer with a passion for Linux and Javascript.
+      In my free time, I enjoy solving LeetCode exercises, reading and studying.
+      I am excited to share my skills and experience with others in the field while also learning as much as  I can!
+    </p>
+    </Box>
+    </Item>
+    <Item className="projects" style={{
+      color: "#fff",
+    }}>
+    <Box component="section" sx={{ p: 2}}>
+      <h2 className="fira-mono-medium" style={{ textAlign: "left", }}>My Projects 🖥️</h2> 
+      <p className="fira-mono-regular"> 
+        My projects focus now on making low level applications using c++ and / or rust language or actually just setting up OSes: 
+      </p>
+      <Box component="div" sx={{ p: 2 }}>
+        <ul className="projectsList">
+          <li> <h2 className="fira-mono-medium"><a href="#">Emacs Config</a></h2>
+              <p> Little config made for vanilla emacs aimed to achieve a fully operational CIDE without using frameworks or anything like that. Does not Include Evil </p> 
+          </li>
+          <li> <h2 className="fira-mono-medium"><a href="https://github.com/tarffie/my-unholy-dotfiles?tab=readme-ov-file">Dotfiles</a></h2>
+              <p> My home-baked dotfiles for my linux system and other side projects I did, like a gentoo setup </p> 
+          </li>
+          <li> <h2 className="fira-mono-medium"><a href="#">AutoLinker</a></h2>
+              <p> Application made in C++ inspired by Yay from Arch Linux, made to manipulate tarballs and link then into my /usr/bin directory.</p> 
+          </li>
+        </ul> 
+      </Box>
+    </Box>
+    <Box component="section" sx={{ p: 2}}>
+    <h2 className="fira-mono-medium">My work ️❤️️</h2>
+
+    </Box>
+    </Item>
+    </Grid>
   );
 }
 
